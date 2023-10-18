a3f2c42d0bdf18475581b8a31d16af2123b7a9fc
anyhony58310 <76625426+anyhony58310@users.noreply.github.com>
2023-10-18 15:51:32 +0200

    Organiser le code PHP en bloc

--- index.php
+++ index.php
@@ -11,18 +11,9 @@
     
     <header>
         <!-- Le menu -->
-    
-        <nav id="menu">        
-            <div class="element_menu">
-                <h3>Titre menu</h3>
-                <ul>
-                    <li><a href="page1.html">Lien</a></li>
-                    <li><a href="page2.html">Lien</a></li>
-                    <li><a href="page3.html">Lien</a></li>
-                </ul>
-            </div>    
-        </nav>
-       
+        <?php
+            include('header.php');
+        ?>       
     </header>
     
     <!-- Le corps -->
@@ -38,7 +29,9 @@
     <!-- Le pied de page -->
     
     <footer id="pied_de_page">
-        <p>Copyright moi, tous droits réservés</p>
+    <?php
+            include('footer.php');
+        ?>
     </footer>
     
     </body>
